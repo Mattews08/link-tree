ba1abda0db7dc3d44d5e291eda1bfcbe177ab529
mathewsLima <mattheus.l08@gmail.com>
2024-10-23 08:53:49 -0400

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,53 @@
+import { Button } from "@/components/ui/button";
+import { Instagram, UtensilsCrossed } from "lucide-react";
 import Image from "next/image";
-import localFont from "next/font/local";
+import { BsWhatsapp } from "react-icons/bs";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
-export default function Home() {
+export default function Component() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="min-h-screen bg-[url('https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-yPpBI6Rnu1VcYqslsCe8Ii8wLOfDDG.png')] bg-cover bg-bottom flex flex-col items-center justify-center p-4">
+      <div className="w-full max-w-md flex flex-col items-center bg-white bg-opacity-80 backdrop-blur-sm rounded-3xl p-8 shadow-lg">
         <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src="/logo.JPG"
+          alt="Mindy Frauke"
+          width={80}
+          height={80}
+          className="rounded-full border-4 border-white mb-2 shadow-md"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <h1 className="text-xl font-semibold text-gray-800 mb-1">
+          Memórias de Ana
+        </h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+        <div className="w-full space-y-3 mt-5">
+          <a href="https://wa.me/5592984512933?text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20um%20pedido.">
+            <Button
+              className="w-full bg-white text-black hover:bg-gray-100 flex items-center px-4 py-3 rounded-xl relative shadow-sm transition-all duration-200 ease-in-out hover:shadow-md"
+              variant="ghost"
+            >
+              <BsWhatsapp className="h-6 w-6 absolute left-4 text-[#25D366]" />
+              <span className="flex-grow text-center">WhatsApp</span>
+            </Button>
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+
+          <a href="https://www.ifood.com.br/delivery/manaus-am/memorias-de-ana-coroado/4bbbe952-1550-41ed-b52a-5467c4a7d1be?UTM_Medium=share">
+            <Button
+              className="w-full bg-white text-black hover:bg-gray-100 flex items-center px-4 py-3 rounded-xl relative shadow-sm transition-all duration-200 ease-in-out hover:shadow-md"
+              variant="ghost"
+            >
+              <UtensilsCrossed className="h-6 w-6 absolute left-4 text-[#EA1D2C]" />
+              <span className="flex-grow text-center">iFood</span>
+            </Button>
           </a>
+
+          <Button
+            className="w-full bg-white text-black hover:bg-gray-100 flex items-center px-4 py-3 rounded-xl relative shadow-sm transition-all duration-200 ease-in-out hover:shadow-md"
+            variant="ghost"
+          >
+            <Instagram className="h-6 w-6 absolute left-4 text-[#E4405F]" />
+            <span className="flex-grow text-center">Instagram</span>
+          </Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
